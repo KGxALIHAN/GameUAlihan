50c68a292401cd368c451672aef2a9cf3d98ab07
KGxALIHAN <ulugbekovalihan1@gmail.com>
2024-10-11 00:36:35 +0600

    добавил архимага

--- lesson_4.py
+++ lesson_4.py
@@ -1,5 +1,5 @@
-from random import randint, choice
-
+from random import randint, choice, sample
+import time
 
 class GameEntity:
     def __init__(self, name, health, damage):
@@ -44,13 +44,14 @@ class Boss(GameEntity):
         self.__defence = random_hero.ability
 
     def attack(self, heroes_list):
-        for hero in heroes_list:
-            if hero.health > 0:
-                if type(hero) == Berserk and self.__defence != hero.ability:
-                    hero.blocked_damage = choice([5, 10])
-                    hero.health -= (self.damage - hero.blocked_damage)
-                else:
-                    hero.health -= self.damage
+        alive_heroes = [hero for hero in heroes_list if hero.health > 0]
+        targets = sample(alive_heroes, min(3, len(alive_heroes)))  # Выбор до 3 случайных героев
+        for hero in targets:
+            if type(hero) == Berserk and self.__defence != hero.ability:
+                hero.blocked_damage = choice([5, 10])
+                hero.health -= (self.damage - hero.blocked_damage)
+            else:
+                hero.health -= self.damage
 
     @property
     def defence(self):
@@ -91,7 +92,7 @@ class Magic(Hero):
         super().__init__(name, health, damage, 'BOOST')
 
     def apply_super_power(self, boss, heroes_list):
-        # TODO here will be implementation of boosting
+        # TODO здесь будет реализация увеличения атаки
         pass
 
 
@@ -124,6 +125,50 @@ class Medic(Hero):
                 hero.health += self.__heal_points
 
 
+class Witcher(Hero):
+    def __init__(self, name, health, damage):
+        super().__init__(name, health, damage, 'REVIVE')
+
+    def apply_super_power(self, boss, heroes_list):
+        for hero in heroes_list:
+            if hero.health <= 0:
+                print(f'{self.name} sacrifices himself to revive {hero.name}.')
+                hero.health = hero.health + 100  # Примерное количество здоровья при воскрешении
+                self.health = 0  # Умирает сам
+
+
+class Avrora(Hero):
+    def __init__(self, name, health, damage):
+        super().__init__(name, health, damage, 'INVISIBILITY')
+        self.invisible = False
+        self.turns_invisible = 0
+
+    def apply_super_power(self, boss, heroes_list):
+        if not self.invisible:
+            self.invisible = True
+            self.turns_invisible = 2
+            print(f'{self.name} has entered invisibility mode!')
+        elif self.turns_invisible > 0:
+            self.turns_invisible -= 1
+        else:
+            self.invisible = False
+            print(f'{self.name} is now visible!')
+
+
+class Megumin(Hero):
+    def __init__(self, name, health, damage):
+        super().__init__(name, health, damage, 'EXPLOSION')
+        self.can_use_ultimate = True
+
+    def apply_super_power(self, boss, heroes_list):
+        if self.can_use_ultimate and self.health <= 100:
+            print(f'{self.name} is preparing to cast a spell...')
+            time.sleep(5)  # Задержка для прочтения заклинания
+            boss.health -= 2000
+            print(f'{self.name} casts a spell dealing 2000 damage to the boss!')
+            self.can_use_ultimate = False  # Можно использовать только один раз
+
+
 round_number = 0
 
 
@@ -131,11 +176,7 @@ def is_game_over(boss, heroes_list):
     if boss.health <= 0:
         print('Heroes won!!!')
         return True
-    all_heroes_dead = True
-    for hero in heroes_list:
-        if hero.health > 0:
-            all_heroes_dead = False
-            break
+    all_heroes_dead = all(hero.health <= 0 for hero in heroes_list)
     if all_heroes_dead:
         print('Boss won!!!')
         return True
@@ -154,15 +195,18 @@ def play_round(boss, heroes_list):
     round_number += 1
     boss.choose_defence(heroes_list)
     boss.attack(heroes_list)
+    
     for hero in heroes_list:
         if hero.health > 0 and boss.health > 0 and boss.defence != hero.ability:
             hero.attack(boss)
             hero.apply_super_power(boss, heroes_list)
+    
     show_statistics(boss, heroes_list)
+    time.sleep(3)  # Пауза между раундами
 
 
 def start_game():
-    boss = Boss(name='Minotavr', health=1000, damage=50)
+    boss = Boss(name='Minotavr', health=6000, damage=50)
 
     warrior_1 = Warrior(name='Asterix', health=290, damage=10)
     warrior_2 = Warrior(name='Obelix', health=280, damage=15)
@@ -170,8 +214,11 @@ def start_game():
     berserk = Berserk(name='Guts', health=220, damage=10)
     doc = Medic(name='Doc', health=200, damage=5, heal_points=15)
     assistant = Medic(name='Junior', health=300, damage=5, heal_points=5)
+    witcher = Witcher(name='Witcher', health=250, damage=15)
+    avrora = Avrora(name='Avrora', health=300, damage=10)
+    megumin = Megumin(name='Megumin', health=500, damage=0)
 
-    heroes_list = [warrior_1, doc, warrior_2, magic, berserk, assistant]
+    heroes_list = [warrior_1, doc, warrior_2, magic, berserk, assistant, witcher, avrora, megumin]
     show_statistics(boss, heroes_list)
 
     while not is_game_over(boss, heroes_list):
